2190669eecd8f2483435eb2f81029504dc1ea4c9
alabdal3 <alabdal3@msu.edu>
2025-03-31 17:30:37 +0000

    Added new logo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Logo from '/Logo.png'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+        <img src={Logo} className="logo" alt="Logo" />
+        
+        <h1>How's your Mood Today?</h1>
+        
+    </>
+  )
+}
+
+export default App
