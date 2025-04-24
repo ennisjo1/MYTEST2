17d438e3261323ff471b1b56b5ab7a927159dff6
ennisjo <ennisjo1@msu.edu>
2025-04-24 22:10:24 +0000

    Josh Headshot

--- src/SpinningImage.jsx
+++ src/SpinningImage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SpinningImage.css";
 import Ibrahim from "./assets/headshot.svg";
+import Josh from ".assets/Joshheadshot.png";
 
 const SpinningImage = () => {
   return (
     <div className="spinner-container">
-      <img src={Ibrahim} alt="Spinning" className="spin" />
+      <img src={Ibrahim} alt="Spinning head of Website Creator Ibrahim" className="spin" />
+      <img src={Josh} alt="Spinning head of Creator Joshua" className="spin" />
     </div>
   );
 };
