2744e9d15051c8a50a29a023cf49e993586e7aa0
alabdal3 <alabdal3@msu.edu>
2025-04-25 16:03:07 +0000

    Luke added

--- src/SpinningImage.jsx
+++ src/SpinningImage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./SpinningImage.css";
 import Ibrahim from "./assets/headshot.svg";
 import Josh from "./assets/Joshheadshot.png";
+import Luke from "./assets/Luke.png";
 
 const SpinningImage = () => {
   return (
     <div className="spinner-container">
       <img src={Ibrahim} alt="Spinning head of Website Creator Ibrahim" className="spin" />
       <img src={Josh} alt="Spinning head of Creator Joshua" className="spin" />
+      <img src={Luke} alt="Spinning head of Creator Luke" className="spin" />
     </div>
   );
 };
